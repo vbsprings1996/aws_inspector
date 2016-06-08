5ba001234193938fd03f03c5ac8c4a1f4133e383
Vijay Bheemineni <vbsprings1996@gmail.com>
2016-06-07 21:52:51 -0500

    Commented logging related lines, as its not working.

--- recipes/aws_inspector_user.rb
+++ recipes/aws_inspector_user.rb
@@ -4,14 +4,18 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-#Creating the group 'hosting'. Currently hard coding but refactor the code.
+# Creating the group 'hosting'. Currently hard coding but refactor the code.
 group node.default['aws_inspector']['group'] do
+  #Chef::Log.info("INFO : Creating group :
+                #{node.default['aws_inspector']['group']}")
 end
 
-###Creating the user 'hosting'. Currently hard coding but refactor the code.
-#Defining 'user' home directory.
-#user node.default['aws_inspector']['user'] do
+### Creating the user 'hosting'. Currently hard coding but refactor the code.
+# Defining 'user' home directory.
+# user node.default['aws_inspector']['user'] do
 user node.default['aws_inspector']['group'] do
+  #Chef::Log.info("INFO : Creating user :
+                #{node.default['aws_inspector']['user']}")
   group node.default['aws_inspector']['group']
   shell '/bin/bash'
   comment 'aws inspector user'

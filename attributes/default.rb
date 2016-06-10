1ade3ccbd16496b167db389e4ea2edf4e7713874
Vijay Bheemineni <vbsprings1996@gmail.com>
2016-06-10 13:30:14 -0500

    1) Removed hard coding of path and defined attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,3 +10,5 @@ default['aws_inspector']['install_script'] = 'install'
 # Used in aws_inspector_install_script_gpg_signature_validation.rb recipe.
 default['aws_inspector']['install_script_gpg_public_key'] = 'https://d1wk0tztpsntt1.cloudfront.net/linux/latest/inspector.gpg'
 default['aws_inspector']['install_script_gpg_signature_file'] = 'https://d1wk0tztpsntt1.cloudfront.net/linux/latest/install.sig'
+default['aws_inspector']['gpg_public_key'] = 'inspector.gpg'
+default['aws_inspector']['install_script_sig'] = 'install.sig'
--- recipes/aws_inspector_install_script_gpg_signature_validation.rb
+++ recipes/aws_inspector_install_script_gpg_signature_validation.rb
@@ -14,20 +14,22 @@ package 'install_gpg' do
   end
 end
 
-
+# AWS inspector gpg public key path.
+aws_inspector_gpg_public_key_path = "#{node.default['aws_inspector']['resources_download_dir']}/#{node.default['aws_inspector']['gpg_public_key']}"
 # Download aws_inspector gpg public key.
-remote_file '/tmp/inspector.gpg' do
+remote_file aws_inspector_gpg_public_key_path do
   source node.default['aws_inspector']['install_script_gpg_public_key']
   owner node.default['aws_inspector']['group']
   group node.default['aws_inspector']['group']
   mode '0755'
 end
 
+
 # Import the downloaded aws_inspector public key.
 ruby_block "gpg_import_public_key_and_store_key_value" do
     block do
         Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
-        gpg_command = "gpg --import /tmp/inspector.gpg"
+        gpg_command = "gpg --import #{aws_inspector_gpg_public_key_path}"
         gpg_command_out = shell_out(gpg_command)
     end
     action :run
@@ -50,8 +52,9 @@ ruby_block "gpg_verify_aws_inspector_fingerprint" do
     action :run
 end
 
+aws_inspector_install_script_signature_path = "#{node.default['aws_inspector']['resources_download_dir']}/#{node.default['aws_inspector']['install_script_sig']}"
 # Download aws_inspector install script signature file.
-remote_file '/tmp/install.sig' do
+remote_file aws_inspector_install_script_signature_path do
   source node.default['aws_inspector']['install_script_gpg_signature_file']
   owner node.default['aws_inspector']['group']
   group node.default['aws_inspector']['group']
@@ -62,7 +65,7 @@ end
 ruby_block "gpg_verify_aws_inspector_install_script_signature" do
     block do
         Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
-        gpg_command = "gpg --verify /tmp/install.sig 2>&1 | grep -i 'Amazon' | grep -i 'good'"
+        gpg_command = "gpg --verify #{aws_inspector_install_script_signature_path} 2>&1 | grep -i 'Amazon' | grep -i 'good'"
         gpg_command_out = shell_out(gpg_command)
         puts "\nINFO: ***gpg_command_out.stdout : #{gpg_command_out.stdout}"
         if gpg_command_out.stdout =~ /Good signature/
